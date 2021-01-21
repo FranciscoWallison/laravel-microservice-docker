5b56eb241cc103b506140a817752f67084084b07
Francisco Wallison <franciscowallison@gmail.com>
2021-01-21 08:55:03 -0300

    Add except creat end edit of Catefories of Api

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['namespace' => 'Api'], function(){
-    Route::resource('categories', 'CategoryController');
+    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
 });
