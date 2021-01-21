8ac9029e57fcacb7bcfab13ece9e82a73085da3e
Francisco Wallison <franciscowallison@gmail.com>
2021-01-21 08:23:11 -0300

    Criand grupos de Rotas Api routes

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('categories', 'Api\CategoryController');
+Route::group(['namespace' => 'Api'], function(){
+    Route::resource('categories', 'CategoryController');
+});
