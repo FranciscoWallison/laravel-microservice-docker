c3f2b4248f1385999cd16929433407b8e4234646
Francisco Wallison <franciscowallison@gmail.com>
2021-01-23 20:18:44 -0300

    Melhorando abstração de TestSaves

--- tests/Traits/TestSaves.php
+++ tests/Traits/TestSaves.php
@@ -11,17 +11,24 @@ use Illuminate\Support\Facades\Lang;
 trait TestSaves
 {
     protected function assertStore(
-        $sendData,
-        $testData
-    )
+        array $sendData,
+        $testDatabase,
+        $testJsonData = null
+    ) : TestResponse
     {
+        
         $response = $this->json('POST', $this->routeStore(), $sendData);
         if($response->status() !== 201){
             throw new \Exception("Response status must be 201, give {$response->status()}: \n {$response->constant()}");
         }
         $model = $this->model();
         $table = (new $model)->getTable();
-        $this->assertDatabaseHas($table, $testData + ['id' => $response->json('id')]);
+        $idResponseJson = ['id' => $response->json('id')];
+        $this->assertDatabaseHas($table, $testDatabase + $idResponseJson);
+
+        $testResponse = $testJsonData ?? $testDatabase;
+        $response->assertJsonFragment($testResponse + $idResponseJson);
+        return $response;
     }
 
 }
