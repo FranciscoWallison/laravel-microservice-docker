d3bb4dda0928d1b17c2ee6c367f17321d13b6819
Francisco Wallison <franciscowallison@gmail.com>
2021-01-23 18:53:43 -0300

    Iiniciando otimização de teste de Genero

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -15,25 +15,31 @@ class CategoryControllerTest extends TestCase
 {
     use DatabaseMigrations, TestValidations;
 
+    private $category;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->category = factory(Category::class)->create(); 
+    }
+
     public function testIndexList()
     {
-        $category = factory(Category::class)->create(); 
         $response = $this->get(route('categories.index'));
 
         $response
         ->assertStatus(200)
-        ->assertJson([$category->toArray()]);
+        ->assertJson([$this->category->toArray()]);
     }
 
 
     public function testShow()
     {
-        $category = factory(Category::class)->create(); 
-        $response = $this->get(route('categories.show', ['category' => $category->id]));
+        $response = $this->get(route('categories.show', ['category' => $this->category->id]));
 
         $response
         ->assertStatus(200)
-        ->assertJson($category->toArray());
+        ->assertJson($this->category->toArray());
     }
 
     //Falhas
@@ -44,14 +50,17 @@ class CategoryControllerTest extends TestCase
             'name' => ''
         ];
         $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
         $data = [
             'name'=> str_repeat('a', 256)
         ];
         $this->assertInvalidationInStoreAction($data, 'max.string', ['max' => 255]);
+        $this->assertInvalidationInUpdateAction($data, 'max.string', ['max' => 255]);
         $data = [
             'is_active' => 'a'
         ];
         $this->assertInvalidationInStoreAction($data, 'boolean');
+        $this->assertInvalidationInUpdateAction($data, 'boolean');
 
 
 
@@ -102,10 +111,9 @@ class CategoryControllerTest extends TestCase
 
     public function testInvalidationDataIsActiveUpdate()
     {
-        $category = factory(Category::class)->create(); 
         $response = $this->json(
             'PUT', 
-            route('categories.update',['category' => $category->id]), 
+            route('categories.update',['category' => $this->category->id]), 
             [
                 'name' => str_repeat('a', 256),
                 'is_active' => 'a'
@@ -204,11 +212,10 @@ class CategoryControllerTest extends TestCase
 
     public function testDestroy()
     {
-        $category = $category = factory(Category::class)->create();
-        $response = $this->json('DELETE', route('categories.destroy', ['category' => $category->id]));
+        $response = $this->json('DELETE', route('categories.destroy', ['category' => $this->category->id]));
         $response->assertStatus(204);
-        $this->assertNull(Category::find($category->id));
-        $this->assertNotNull(Category::withTrashed()->find($category->id));
+        $this->assertNull(Category::find($this->category->id));
+        $this->assertNotNull(Category::withTrashed()->find($this->category->id));
     }
 
     // Teste chamadas
@@ -249,6 +256,11 @@ class CategoryControllerTest extends TestCase
     protected function routeStore()
     {
         return route('categories.store');
+    }
+
+    protected function routeUpdate()
+    {
+        return route('categories.update', ['category'=> $this->category->id]);
     }
 
 }
