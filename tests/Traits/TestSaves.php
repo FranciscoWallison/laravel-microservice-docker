d61139ee08e2bd13caebeba17c0f79fe8ef154d5
Francisco Wallison <franciscowallison@gmail.com>
2021-01-23 21:12:49 -0300

    Criando update TestSaves

--- tests/Traits/TestSaves.php
+++ tests/Traits/TestSaves.php
@@ -21,14 +21,43 @@ trait TestSaves
         if($response->status() !== 201){
             throw new \Exception("Response status must be 201, give {$response->status()}: \n {$response->constant()}");
         }
+       
+        $this->assertInDatabase($response, $testDatabase);
+        $this->assertJsonResponseContent($response, $testDatabase, $testJsonData);
+        return $response;
+    }
+
+
+    protected function assertUpdate(
+        array $sendData,
+        $testDatabase,
+        $testJsonData = null
+    ) : TestResponse
+    {
+        $response = $this->json('PUT', $this->routeUpdate(), $sendData);
+        if($response->status() !== 200){
+            throw new \Exception("Response status must be 200, give {$response->status()}: \n {$response->constant()}");
+        }
+       
+        $this->assertInDatabase($response, $testDatabase);
+        $this->assertJsonResponseContent($response, $testDatabase, $testJsonData);
+        return $response;
+    }
+
+    private function assertInDatabase(TestResponse $response, $testDatabase)
+    {
         $model = $this->model();
         $table = (new $model)->getTable();
         $idResponseJson = ['id' => $response->json('id')];
         $this->assertDatabaseHas($table, $testDatabase + $idResponseJson);
+    }
 
+
+    private function assertJsonResponseContent(TestResponse $response, $testDatabase, $testJsonData = null)
+    {
+        $idResponseJson = ['id' => $response->json('id')];
         $testResponse = $testJsonData ?? $testDatabase;
         $response->assertJsonFragment($testResponse + $idResponseJson);
-        return $response;
     }
 
 }
