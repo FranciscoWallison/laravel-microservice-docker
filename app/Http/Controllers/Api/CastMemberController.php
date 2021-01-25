f8c6e15f170a0ffc211b27afff5d1e33841a1c18
Francisco Wallison <franciscowallison@gmail.com>
2021-01-25 07:01:00 -0300

    Removendo chamadas inutilizadas

--- app/Http/Controllers/Api/CastMemberController.php
+++ app/Http/Controllers/Api/CastMemberController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\CastMember;
-use Illuminate\Http\Request;
 
 class CastMemberController extends BasicCrudController
 {
