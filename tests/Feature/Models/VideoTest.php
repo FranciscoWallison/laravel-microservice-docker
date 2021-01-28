ba866e66bddb934e7d3bbacf4398f88329aafc85
Francisco Wallison <franciscowallison@gmail.com>
2021-01-28 19:25:49 -0300

    Criando teste do model feature

--- tests/Feature/Models/VideoTest.php
+++ tests/Feature/Models/VideoTest.php
@@ -2,21 +2,63 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Video;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class VideoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+    
+    public function testRollbackCreate()
     {
-        $response = $this->get('/');
+        $hasError = false;
+        try {
+           Video::create([
+            "title" => "Velit quasi autem dicta.",
+            "description" => "Fugit iste rerum eos et molestias voluptatibus occaecati ad at velit deserunt distinctio sint.",
+            "year_launched" => 2007,
+            "opened" => true,
+            "rating" => Video::RATING_LIST[0],
+            "duration" => 25,
+            'categories_id' => [0,1,2,3]
+           ]); 
 
-        $response->assertStatus(200);
+        } catch ( QueryException $exception ){
+            $this->assertCount(0, Video::all());
+            $hasError = true;
+        }
+
+        $this->assertTrue($hasError);
+    }
+
+
+    public function testRollbackUpdate()
+    {
+        $hasError = false;
+        $video = factory(Video::class)->create();
+        $oldTitle = $video->title;
+        try {
+           
+            $video->update([
+                "title" => "Velit quasi autem dicta.",
+                "description" => "Fugit iste rerum eos et molestias voluptatibus occaecati ad at velit deserunt distinctio sint.",
+                "year_launched" => 2007,
+                "opened" => true,
+                "rating" => Video::RATING_LIST[0],
+                "duration" => 25,
+                'categories_id' => [0,1,2,3]
+            ]); 
+        } catch ( QueryException $exception ){
+            $this->assertDatabaseHas('videos', [
+                'title' =>  $oldTitle
+            ]);
+            $hasError = true;
+        }
+
+        $this->assertTrue($hasError);
     }
 }
