c1b4f561dd6ebdcf78e065ccfd0b75a2204ef82b
Francisco Wallison <franciscowallison@gmail.com>
2021-02-28 14:36:23 -0300

    API Resource com action index

--- app/Http/Controllers/Api/BasicCrudController.php
+++ app/Http/Controllers/Api/BasicCrudController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 abstract class BasicCrudController extends Controller
 {
+    protected $paginationSize = 15;
     protected abstract function model();
     protected abstract function rulesStore();
     protected abstract function rulesUpdate();
     protected abstract function resource();
-    
+    protected abstract function resourceCollection();
     
     public function index()
     {
-        return $this->model()::all();
+        $data = !$this->paginationSize ? $this->model()::all(): $this->model()::paginate($this->paginationSize);
+        $resourceCollectionClass = $this->resourceCollection();
+        $refClass = new \ReflectionClass($resourceCollectionClass);
+        return $refClass->isSubclassOf(AnonymousResourceCollection::class)
+            ? new $resourceCollectionClass($data)
+            : $resourceCollectionClass::collection($data);
     }
 
     public function store(Request $request)
