681bee2e62a16740097beef2df608187399cf11d
Francisco Wallison <franciscowallison@gmail.com>
2021-03-04 12:47:37 -0300

    Iniciando aplicação ReactJs

--- .docker/entrypointClient.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-# chown -R www-data:www-data .
-npm config set cache /var/www/.npm-cache --global
