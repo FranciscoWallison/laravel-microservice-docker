504ccac286a965bd88c0785f122af484a7c66373
Francisco Wallison <franciscowallison@gmail.com>
2021-03-05 06:40:03 -0300

    Iniciando com hook

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -24,7 +24,11 @@ type Props = {
 };
 export const Navbar = (props: Props) => {
     const classes = useStyles();
-    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleOpen = (event:any) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     return (
         
         <AppBar>
@@ -35,14 +39,22 @@ export const Navbar = (props: Props) => {
                     aria-label="open drawer" 
                     aria-controls="menu-appbar" 
                     aria-haspopup="true"
+                    onClick={handleOpen}
                 >
                         <MenuIcon/>
                 </IconButton>
                 <Menu
                     id="menu-appbar"
-                    open={false}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                    transformOrigin={{vertical: 'top', horizontal: 'center'}}
+                    getContentAnchorEl={null}
                 >
-                    <MenuItem>
+                    <MenuItem
+                        onClick={handleClose}
+                    >
                         Categoria
                     </MenuItem>
                 </Menu>
