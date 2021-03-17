aa489c78f453e61ae34656f18cdaf51a678441d7
Francisco Wallison <franciscowallison@gmail.com>
2021-03-17 08:45:15 -0300

    frontend/src/Criando Componente De Listagem De Categorias

---
+++ frontend/src/pages/category/Table.tsx
@@ -0,0 +1,15 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+
+const Table = (props: Props) => {
+    return (
+        <div>
+            Table
+        </div>
+    );
+};
+
+export default Table;
