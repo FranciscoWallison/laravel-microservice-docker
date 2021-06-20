6383a09485986e5827d4f2f37fdc44f17fae97c9
Francisco Wallison <franciscowallison@gmail.com>
2021-06-20 11:49:54 -0300

    Corrigindo chamada search

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes, Traits\Uuid;
+    use SoftDeletes, Traits\Uuid, Filterable;
     // Quais os campos são permitidos
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
@@ -16,4 +17,14 @@ class Category extends Model
         'is_active' => 'boolean'
     ];
     public $incrementing = false;
+
+    public function modelFiler()
+    {
+        return $this->provideFilter(CategoryFilter::class);
+    }
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class)->withTrashed();
+    }
 }
