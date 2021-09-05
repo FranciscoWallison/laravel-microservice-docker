1c46450b286b535800867a627e354ca1e26aa1cc
Francisco Wallison <franciscowallison@gmail.com>
2021-09-05 10:05:43 -0300

    Normalização do conceito de search e filter

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -11,7 +11,7 @@ import { IconButton, MuiThemeProvider } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import EditIcon from '@material-ui/icons/Edit';
 import { FilterResetButton } from '../../components/Table/FilterResetButton';
-import reducer, {INITIAL_STATE, Creators} from '../../store/search';
+import reducer, {INITIAL_STATE, Creators} from '../../store/filter';
 
 const columnsDefinition: MUIDataTableColumn[] = [
     {
