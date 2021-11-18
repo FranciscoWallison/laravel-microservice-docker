0985938d281d72236d1a33f495501b593141c7ba
Francisco Wallison <franciscowallison@gmail.com>
2021-11-18 15:17:34 -0300

    add uploaprogress

---
+++ frontend/src/components/UploadProgress.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+
+import { FileUpload, Upload } from "../store/upload/types";
+import { hasError } from "../store/upload/getters";
+
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { CircularProgress, Fade } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme: Theme) => ({
+
+    progressContainer: {
+        position: 'relative'
+    },
+    progress: {
+        position: 'absolute',
+        left: 0
+    },
+    progressBackground: {
+        color: grey["300"]
+    }
+
+}));
+
+interface UploadProgressProps {
+    uploadOrFile: Upload | FileUpload;
+    size: number;
+}
+
+const UploadProgress: React.FC<UploadProgressProps> = (props) => {
+    const classes = useStyles();
+    const { size, uploadOrFile } = props;
+
+    const error = hasError(uploadOrFile);
+    return (
+        <Fade in={uploadOrFile.progress < 1} timeout={{ enter: 100, exit: 2000 }}>
+            <div className={classes.progressContainer}>
+                <CircularProgress
+                    variant={"static"}
+                    value={100}
+                    className={classes.progressBackground}
+                    size={size}
+                />
+
+                <CircularProgress
+                    className={classes.progress}
+                    variant={"static"}
+                    value={error ? 0 : uploadOrFile.progress * 100}
+                    size={size}
+                />
+
+            </div>
+        </Fade>
+    );
+};
+
+export default UploadProgress;
